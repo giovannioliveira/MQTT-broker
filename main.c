5a57f4758b5d80b052e82d3b60ec2b357f1819b7
Giovanni Oliveira <giovanni.oliveira@ab-inbev.com>
2021-09-19 18:05:33 -0300

    documenting

--- main.c
+++ main.c
@@ -1,261 +1,104 @@
-/* Por Prof. Daniel Batista <batista@ime.usp.br>
- * Em 4/4/2021
- * 
- * Um código simples de um servidor de eco a ser usado como base para
- * o EP1. Ele recebe uma linha de um cliente e devolve a mesma linha.
- * Teste ele assim depois de compilar:
- * 
- * ./mac5910-servidor-exemplo-ep1 8000
- * 
- * Com este comando o servidor ficará escutando por conexões na porta
- * 8000 TCP (Se você quiser fazer o servidor escutar em uma porta
- * menor que 1024 você precisará ser root ou ter as permissões
- * necessáfias para rodar o código com 'sudo').
- *
- * Depois conecte no servidor via telnet. Rode em outro terminal:
- * 
- * telnet 127.0.0.1 8000
- * 
- * Escreva sequências de caracteres seguidas de ENTER. Você verá que o
- * telnet exibe a mesma linha em seguida. Esta repetição da linha é
- * enviada pelo servidor. O servidor também exibe no terminal onde ele
- * estiver rodando as linhas enviadas pelos clientes.
- * 
- * Obs.: Você pode conectar no servidor remotamente também. Basta
- * saber o endereço IP remoto da máquina onde o servidor está rodando
- * e não pode haver nenhum firewall no meio do caminho bloqueando
- * conexões na porta escolhida.
- */
+#include "main.h"
 
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <time.h>
-#include <unistd.h>
-#include <pthread.h>
-#include "lib/set.h"
+int main (int argc, char **argv) {
 
-#define LISTENQ 1
-#define MAXDATASIZE 100
-#define MAXLINE 4096
-#define MAXCLIENTS 256
+    initThreadSlots();
 
-#define PORT 1883
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+    /* Os sockets. Um que será o socket que vai escutar pelas conexões
+     * e o outro que vai ser o socket específico de cada conexão */
+    int listenfd, connfd;
+    /* Informações sobre o socket (endereço e porta) ficam nesta struct */
+    struct sockaddr_in servaddr;
 
-
-
-enum control_packet_types {
-    CONNECT=1,
-    CONNACK=2,
-    PUBLISH=3,
-    PUBACK=4,
-    PUBREC=5,
-    PUBREL=6,
-    PUBCOMP=7,
-    SUBSCRIBE=8,
-    SUBACK=9,
-    UNSUBSCRIBE=10,
-    UNSUBACK=11,
-    PINGREQ=12,
-    PINGRESP=13,
-    DISCONNECT=14
-};
-
-const unsigned char flag_bits[] = {(unsigned char) -1,
-                         (unsigned char) 0,
-                         (unsigned char) 0,
-                         (unsigned char) 0,
-                         (unsigned char) 0,
-                         (unsigned char) 0,
-                         (unsigned char) 2,
-                         (unsigned char) 0,
-                         (unsigned char) 2,
-                         (unsigned char) 0,
-                         (unsigned char) 2,
-                         (unsigned char) 0,
-                         (unsigned char) 0,
-                         (unsigned char) 0,
-                         (unsigned char) 0};
-
-const unsigned char variableHeaderLen[] = {
-        -1,
-        10, // CONNECT
-        0,
-        2, // PUBLISH
-        0,
-        0,
-        0,
-        0,
-        2,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-};
-
-enum connection_statuses {
-    DISCONNECTED=0,
-    CONNECTED=1,
-
-};
-
-typedef struct {
-    int connfd;
-    pthread_t thread;
-    char *id;
-    enum connection_statuses connstat;
-    simple_set subscriptions;
-} thread_t;
-
-thread_t threads[MAXCLIENTS];
-int client_count;
-
-void initThreadSlots(){
-    int i;
-    for(i = 0; i<MAXCLIENTS; i++){
-        threads[i].connfd = 0;
-        set_init(&threads[i].subscriptions);
+    if (argc != 2) {
+        fprintf(stderr,"Usage: mqtt-broker [port]\n");
+        exit(1);
     }
-    client_count = 0;
-}
 
-int getNewThreadSlot(int connfd){
-    if(client_count>=MAXCLIENTS){
-        printf("unable to found available slot for connection %d\n",connfd);
-        return -1;
-    }else{
-        int i;
-        pthread_mutex_lock(&lock);
-        for(i=0;i<MAXCLIENTS;i++){
-            if(!threads[i].connfd){
-                threads[i].connfd = connfd;
-                break;
-            }
+    /* Criação de um socket. É como se fosse um descritor de arquivo.
+     * É possível fazer operações como read, write e close. Neste caso o
+     * socket criado é um socket IPv4 (por causa do AF_INET), que vai
+     * usar TCP (por causa do SOCK_STREAM), já que o MQTT funciona sobre
+     * TCP, e será usado para uma aplicação convencional sobre a Internet
+     * (por causa do número 0) */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket :(\n");
+        exit(2);
+    }
+
+    /* Agora é necessário informar os endereços associados a este
+     * socket. É necessário informar o endereço / interface e a porta,
+     * pois mais adiante o socket ficará esperando conexões nesta porta
+     * e neste(s) endereços. Para isso é necessário preencher a struct
+     * servaddr. É necessário colocar lá o tipo de socket (No nosso
+     * caso AF_INET porque é IPv4), em qual endereço / interface serão
+     * esperadas conexões (Neste caso em qualquer uma -- INADDR_ANY) e
+     * qual a porta. Neste caso será a porta que foi passada como
+     * argumento no shell (atoi(argv[1]))
+     */
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(atoi(argv[1]));
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+        perror("bind :(\n");
+        exit(3);
+    }
+
+    /* Como este código é o código de um servidor, o socket será um
+     * socket passivo. Para isto é necessário chamar a função listen
+     * que define que este é um socket de servidor que ficará esperando
+     * por conexões nos endereços definidos na função bind. */
+    if (listen(listenfd, LISTENQ) == -1) {
+        perror("listen :(\n");
+        exit(4);
+    }
+
+    printf("[Servidor no ar. Aguardando conexões na porta %d]\n",atoi(argv[1]));
+    printf("[Para finalizar, pressione CTRL+c ou rode um kill ou killall]\n");
+
+
+
+    /* O servidor no final das contas é um loop infinito de espera por
+     * conexões e processamento de cada uma individualmente */
+    for (;;) {
+        /* O socket inicial que foi criado é o socket que vai aguardar
+         * pela conexão na porta especificada. Mas pode ser que existam
+         * diversos clientes conectando no servidor. Por isso deve-se
+         * utilizar a função accept. Esta função vai retirar uma conexão
+         * da fila de conexões que foram aceitas no socket listenfd e
+         * vai criar um socket específico para esta conexão. O descritor
+         * deste novo socket é o retorno da função accept. */
+        if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
+            perror("accept :(\n");
+            exit(5);
         }
-        client_count++;
-        pthread_mutex_unlock(&lock);
-        printf("slot %d reserved for connection %d\n",i,connfd);
-        return i;
-    }
-}
 
-void releaseThreadSlot(int i){
-
-    pthread_mutex_lock(&lock);
-    threads[i].connfd = 0;
-    threads[i].connstat = DISCONNECTED;
-    free(threads[i].id);
-    set_clear(&threads[i].subscriptions);
-    client_count--;
-    pthread_mutex_unlock(&lock);
-    printf("release slot %d\n",i);
-
-}
-
-void encodeLength(unsigned int x, unsigned char *result){
-    unsigned char encodedByte;
-    do{
-        encodedByte = x % 128;
-        x = x / 128;
-        if( x>0 ){
-            encodedByte |= 128;
+        int slot = getNewThreadSlot(connfd);
+        if(slot == -1){
+            perror("All connection slots are busy.");
+            close(connfd);
         }else{
-            result[0] = encodedByte;
-            result = &result[1];
-        }
-    }while(x>0);
-    result[0] = '\0';
-}
-
-int decodeLength(int connfd){
-    int mult = 1;
-    int value = 0;
-    int iteration = 0;
-    unsigned char encodedByte;
-    do{
-        if(read(connfd,&encodedByte,1)!=1){
-            return -1;
-        }
-        value += (encodedByte & 127) * mult;
-        mult *= 128;
-        if(mult > 128*128*128){
-            return -1;
-        }
-        iteration++;
-    }while((encodedByte & 128) != 0);
-    return value;
-}
-
-void printByteArray(unsigned char *arr, int size){
-    int i;
-    for(i=0;i<size;i++)
-        printf("%X ",arr[i]);
-    printf("\n");
-}
-
-void mqtt_send_message(int thread_from, int thread_to, char *topic_name, char *message){
-    printf("MESSAGE from slot %d;;; to slot %d;;; topic %s;;; message %s\n",thread_from, thread_to,topic_name,message);
-    unsigned long topic_name_len = strlen(topic_name);
-    unsigned long message_len = strlen(message);
-    unsigned long response_len = 4+topic_name_len+message_len;
-    unsigned char response[response_len];
-    response[0] = 0x30;
-    response[1] = response_len-2;
-    response[2] = (unsigned char) topic_name_len >> 4;
-    response[3] = (unsigned char) topic_name_len & 0xFF;
-    memcpy(&response[4],topic_name,topic_name_len);
-    memcpy(&response[4+topic_name_len],message,message_len);
-
-    write(threads[thread_to].connfd,response,response_len);
-
-
-}
-
-void mqtt_publish(int thread_from, char *topic_name, char *message) {
-    printf("PUB topic: %s;;; message: %s\n", topic_name, message);
-    int i, cnt;
-    for (i = 0, cnt = 0; i < MAXCLIENTS && cnt < client_count; i++) {
-        thread_t t = threads[i];
-        if (t.connfd) {
-            cnt++;
-            if (set_contains(&t.subscriptions, topic_name) == SET_TRUE) {
-                mqtt_send_message(thread_from, i, topic_name, message);
-            }
+            pthread_create(&threads[slot].thread, NULL, (void *) handleClient, slot);
         }
 
     }
 }
 
-void mqtt_subscribe(char *topic_name, int thread_index){
-    printf("SUB %s %d\n",topic_name, thread_index);
-    set_add(& (threads[thread_index].subscriptions),topic_name);
-}
 
-void mqtt_disconnect(int thread_index){
-    printf("DISC %d\n",thread_index);
-}
+//---------------------------------------------------------------------------//
+// MAIN CLIENT HANDLER                                                       //
+//---------------------------------------------------------------------------//
 
 void handleClient(int thread_index){
 
-    /* Armazena linhas recebidas do cliente */
-    unsigned char recvline[MAXLINE + 1];
     /* Armazena o tamanho da string lida do cliente */
-    ssize_t n;
     thread_t this_thread = threads[thread_index];
     this_thread.connstat = DISCONNECTED;
     int connfd = this_thread.connfd;
 
-    int user_name_flag, password_flag, will_retain, will_qos, will_flag, clean_session, keep_alive;
+    int will_flag;
+//    int user_name_flag, password_flag, will_retain, will_qos, clean_session, keep_alive;
 
     while(1){
 
@@ -320,13 +163,13 @@ void handleClient(int thread_index){
                 break;
             }
 
-            user_name_flag = variable_header[7] & 0x80;
-            password_flag = variable_header[7] & 0x40;
-            will_retain = variable_header[7] & 0x20;
-            will_qos = variable_header[7] & 0x18;
+//            user_name_flag = variable_header[7] & 0x80;
+//            password_flag = variable_header[7] & 0x40;
+//            will_retain = variable_header[7] & 0x20;
+//            will_qos = variable_header[7] & 0x18;
             will_flag = variable_header[7] & 0x04;
-            clean_session = variable_header[7] & 0x02;
-            keep_alive = (variable_header[8] << 4) + variable_header[9];
+//            clean_session = variable_header[7] & 0x02;
+//            keep_alive = (variable_header[8] << 4) + variable_header[9];
 
             if(will_flag){
                 printf("unsuported will flag");
@@ -426,88 +269,114 @@ void handleClient(int thread_index){
 
 
 
-int main (int argc, char **argv) {
+//---------------------------------------------------------------------------//
+// PROTOCOL ACTIONS                                                          //
+//---------------------------------------------------------------------------//
+void mqtt_send_message(int thread_from, int thread_to, char *topic_name, char *message){
+    printf("MESSAGE from slot %d;;; to slot %d;;; topic %s;;; message %s\n",thread_from, thread_to,topic_name,message);
+    unsigned long topic_name_len = strlen(topic_name);
+    unsigned long message_len = strlen(message);
+    unsigned long response_len = 4+topic_name_len+message_len;
+    unsigned char response[response_len];
+    response[0] = 0x30;
+    response[1] = response_len-2;
+    response[2] = (unsigned char) topic_name_len >> 4;
+    response[3] = (unsigned char) topic_name_len & 0xFF;
+    memcpy(&response[4],topic_name,topic_name_len);
+    memcpy(&response[4+topic_name_len],message,message_len);
 
-    initThreadSlots();
+    write(threads[thread_to].connfd,response,response_len);
+}
 
-    /* Os sockets. Um que será o socket que vai escutar pelas conexões
-     * e o outro que vai ser o socket específico de cada conexão */
-    int listenfd, connfd;
-    /* Informações sobre o socket (endereço e porta) ficam nesta struct */
-    struct sockaddr_in servaddr;
-
-    if (argc != 2) {
-        fprintf(stderr,"Uso: %d <Porta>\n",PORT);
-        fprintf(stderr,"Vai rodar um servidor de echo na porta <Porta> TCP\n");
-        exit(1);
-    }
-
-    /* Criação de um socket. É como se fosse um descritor de arquivo.
-     * É possível fazer operações como read, write e close. Neste caso o
-     * socket criado é um socket IPv4 (por causa do AF_INET), que vai
-     * usar TCP (por causa do SOCK_STREAM), já que o MQTT funciona sobre
-     * TCP, e será usado para uma aplicação convencional sobre a Internet
-     * (por causa do número 0) */
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket :(\n");
-        exit(2);
-    }
-
-    /* Agora é necessário informar os endereços associados a este
-     * socket. É necessário informar o endereço / interface e a porta,
-     * pois mais adiante o socket ficará esperando conexões nesta porta
-     * e neste(s) endereços. Para isso é necessário preencher a struct
-     * servaddr. É necessário colocar lá o tipo de socket (No nosso
-     * caso AF_INET porque é IPv4), em qual endereço / interface serão
-     * esperadas conexões (Neste caso em qualquer uma -- INADDR_ANY) e
-     * qual a porta. Neste caso será a porta que foi passada como
-     * argumento no shell (atoi(argv[1]))
-     */
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family      = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port        = htons(PORT);
-    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-        perror("bind :(\n");
-        exit(3);
-    }
-
-    /* Como este código é o código de um servidor, o socket será um
-     * socket passivo. Para isto é necessário chamar a função listen
-     * que define que este é um socket de servidor que ficará esperando
-     * por conexões nos endereços definidos na função bind. */
-    if (listen(listenfd, LISTENQ) == -1) {
-        perror("listen :(\n");
-        exit(4);
-    }
-
-    printf("[Servidor no ar. Aguardando conexões na porta %d]\n",PORT);
-    printf("[Para finalizar, pressione CTRL+c ou rode um kill ou killall]\n");
-
-
-
-    /* O servidor no final das contas é um loop infinito de espera por
-     * conexões e processamento de cada uma individualmente */
-    for (;;) {
-        /* O socket inicial que foi criado é o socket que vai aguardar
-         * pela conexão na porta especificada. Mas pode ser que existam
-         * diversos clientes conectando no servidor. Por isso deve-se
-         * utilizar a função accept. Esta função vai retirar uma conexão
-         * da fila de conexões que foram aceitas no socket listenfd e
-         * vai criar um socket específico para esta conexão. O descritor
-         * deste novo socket é o retorno da função accept. */
-        if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
-            perror("accept :(\n");
-            exit(5);
-        }
-
-        int slot = getNewThreadSlot(connfd);
-        if(slot == -1){
-            perror("All connection slots are busy.");
-            close(connfd);
-        }else{
-            pthread_create(&threads[slot].thread, NULL, handleClient, slot);
+void mqtt_publish(int thread_from, char *topic_name, char *message) {
+    printf("PUB topic: %s;;; message: %s\n", topic_name, message);
+    int i, cnt;
+    for (i = 0, cnt = 0; i < MAXCLIENTS && cnt < client_count; i++) {
+        thread_t t = threads[i];
+        if (t.connfd) {
+            cnt++;
+            if (set_contains(&t.subscriptions, topic_name) == SET_TRUE) {
+                mqtt_send_message(thread_from, i, topic_name, message);
+            }
         }
 
     }
+}
+
+void mqtt_subscribe(char *topic_name, int thread_index){
+    printf("SUB %s %d\n",topic_name, thread_index);
+    set_add(& (threads[thread_index].subscriptions),topic_name);
+}
+
+void mqtt_disconnect(int thread_index){
+    printf("DISC %d\n",thread_index);
+}
+
+
+
+
+
+//---------------------------------------------------------------------------//
+// AUXILIARY FUNCTIONS                                                       //
+//---------------------------------------------------------------------------//
+
+void initThreadSlots(){
+    int i;
+    for(i = 0; i<MAXCLIENTS; i++){
+        threads[i].connfd = 0;
+        set_init(&threads[i].subscriptions);
+    }
+    client_count = 0;
+}
+
+int getNewThreadSlot(int connfd){
+    if(client_count>=MAXCLIENTS){
+        printf("unable to found available slot for connection %d\n",connfd);
+        return -1;
+    }else{
+        int i;
+        pthread_mutex_lock(&lock);
+        for(i=0;i<MAXCLIENTS;i++){
+            if(!threads[i].connfd){
+                threads[i].connfd = connfd;
+                break;
+            }
+        }
+        client_count++;
+        pthread_mutex_unlock(&lock);
+        printf("slot %d reserved for connection %d\n",i,connfd);
+        return i;
+    }
+}
+
+void releaseThreadSlot(int i){
+
+    pthread_mutex_lock(&lock);
+    threads[i].connfd = 0;
+    threads[i].connstat = DISCONNECTED;
+    free(threads[i].id);
+    set_clear(&threads[i].subscriptions);
+    client_count--;
+    pthread_mutex_unlock(&lock);
+    printf("release slot %d\n",i);
+
+}
+
+int decodeLength(int connfd){
+    int mult = 1;
+    int value = 0;
+    int iteration = 0;
+    unsigned char encodedByte;
+    do{
+        if(read(connfd,&encodedByte,1)!=1){
+            return -1;
+        }
+        value += (encodedByte & 127) * mult;
+        mult *= 128;
+        if(mult > 128*128*128){
+            return -1;
+        }
+        iteration++;
+    }while((encodedByte & 128) != 0);
+    return value;
 }
---
+++ main.h
@@ -0,0 +1,157 @@
+/* Por Giovanni Oliveira <giovanni@ime.usp.br>
+ * Em 19/09/2021
+ *
+ * Um código simples de um servidor de MQTT v3.3.1 implementando exclusivamente QoS = 0 (no máximo, entregar uma vez). Recebe como argumento a porta em que o servidor deve escutar.
+ *
+ * ./mqtt-broker 1883
+ *
+ */
+
+/* Referência:
+ * https://edisciplinas.usp.br/pluginfile.php/6485438/mod_assign/introattachment/0/mac5910-servidor-exemplo-ep1.c
+ * Por Prof. Daniel Batista <batista@ime.usp.br>
+ * Em 4/4/2021
+ *
+ * Um código simples de um servidor de eco a ser usado como base para
+ * o EP1. Ele recebe uma linha de um cliente e devolve a mesma linha.
+ */
+
+
+#ifndef MQTT_BROKER_MAIN_H
+#define MQTT_BROKER_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "lib/set.h"
+
+#define _GNU_SOURCE
+#define LISTENQ 1
+#define MAXCLIENTS 256
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+// MQTT packet types
+enum control_packet_types {
+    CONNECT=1,
+//    CONNACK=2,
+    PUBLISH=3,
+//    PUBACK=4,
+//    PUBREC=5,
+//    PUBREL=6,
+//    PUBCOMP=7,
+    SUBSCRIBE=8,
+//    SUBACK=9,
+//    UNSUBSCRIBE=10,
+//    UNSUBACK=11,
+    PINGREQ=12,
+//    PINGRESP=13,
+    DISCONNECT=14
+};
+
+// MQTT packet type flags
+const unsigned char flag_bits[] = {(unsigned char) -1,
+                                   (unsigned char) 0,
+                                   (unsigned char) 0,
+                                   (unsigned char) 0,
+                                   (unsigned char) 0,
+                                   (unsigned char) 0,
+                                   (unsigned char) 2,
+                                   (unsigned char) 0,
+                                   (unsigned char) 2,
+                                   (unsigned char) 0,
+                                   (unsigned char) 2,
+                                   (unsigned char) 0,
+                                   (unsigned char) 0,
+                                   (unsigned char) 0,
+                                   (unsigned char) 0};
+
+// MQTT packet variable header length
+const unsigned char variableHeaderLen[] = {
+        -1,
+        10, // CONNECT
+        0,
+        2, // PUBLISH
+        0,
+        0,
+        0,
+        0,
+        2,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+};
+
+// MQTT connection statuses
+enum connection_statuses {
+    DISCONNECTED=0,
+    CONNECTED=1,
+};
+
+// MQTT connection struct
+typedef struct {
+    int connfd;
+    pthread_t thread;
+    char *id;
+    enum connection_statuses connstat;
+    simple_set subscriptions;
+} thread_t;
+
+thread_t threads[MAXCLIENTS];
+
+// number of connected clients
+int client_count;
+
+int main (int argc, char **argv);
+
+//---------------------------------------------------------------------------//
+// MAIN CLIENT HANDLER                                                       //
+//---------------------------------------------------------------------------//
+
+// thread code to handle new MQTT connection request
+void handleClient(int thread_index);
+
+//---------------------------------------------------------------------------//
+// PROTOCOL ACTIONS                                                          //
+//---------------------------------------------------------------------------//
+
+// sends message to subscribed node
+void mqtt_send_message(int thread_from, int thread_to, char *topic_name, char *message);
+
+// publishes message to all subscribed nodes
+void mqtt_publish(int thread_from, char *topic_name, char *message);
+
+// subscribes a node into a topic
+void mqtt_subscribe(char *topic_name, int thread_index);
+
+// disconnects a node
+void mqtt_disconnect(int thread_index);
+
+//---------------------------------------------------------------------------//
+// AUXILIARY FUNCTIONS                                                       //
+//---------------------------------------------------------------------------//
+
+// init thread slots with null value
+void initThreadSlots();
+
+// gets a new thread slot for connection `connfd`
+// returns slot index in case of success
+// returns -1 in case of fail
+int getNewThreadSlot(int connfd);
+
+// releases thread slot with index i
+void releaseThreadSlot(int i);
+
+// read bytes from `connfd` to get length encoded in MQTT-fashioned-way
+// return the content length, consuming lenth codes from `connfd`
+int decodeLength(int connfd);
+
+#endif //MQTT_BROKER_MAIN_H
